fbcbf55b73157f48698e600e6f234d26d7f1d64b
ghiath-dev <ghiath.dev@gmail.com>
2024-02-26 15:41:15 +0300

    Update Namespace

--- src/Casts/Username.php
+++ src/Casts/Username.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Holoultek\Capabilities\LaravelCasts;
+namespace Holoultek\LaravelCasts\Casts;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Model;
